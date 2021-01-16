bc55e7f12de62089f2a25fcd19c699d998b458a9
Nancy Nguyen <nguyenn8@oregonstate.edu>
2021-01-16 01:10:27 -0800

    removed some comments

--- warmup1.c
+++ warmup1.c
@@ -3,8 +3,20 @@
 #include "user.h"
 
 int
-main(void)
+main(int argc, char *argv[])
 {
+  //int count = 0;
+  //int temp = getsyscallinfo();
+
+  //int count2 = 0; 
+  //if(getsyscallinfo() > 0){
+ // count++;
+ // printf(1, "Number of system calls: %d\n", count);
+ // printf(1, "temp: %d\n", temp);
+  //exit();
+ // }
+
+  //printf(1,"Num of syscall: %d\n", syscall()); 
   printf(1,"Number of system calls: %d\n", getsyscallinfo());
   exit();
 }
